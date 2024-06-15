63529ea3c07dd56aef7b109e6c9db0f22773401d
kuriakosant <kuriakosant2003@gmail.com>
2024-06-16 02:34:32 +0300

    inittial commit

---
+++ backend/data/populateProducts.js
@@ -0,0 +1,34 @@
+// data/populateProducts.js
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Product = require('../models/productModel');
+const products = require('./products');
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+const importData = async () => {
+  try {
+    await Product.deleteMany();
+    await Product.insertMany(products);
+    console.log('Data Imported!');
+    process.exit();
+  } catch (error) {
+    console.error(`Error adding products: ${error}`);
+    process.exit(1);
+  }
+};
+
+connectDB().then(importData);
